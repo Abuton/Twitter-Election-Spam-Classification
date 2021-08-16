5a7bbe8bdb2d719c692bd610d9a3e24c742810e4
Abuton <alaroabubakarolayemi@yahoo.com>
2021-08-16 01:22:05 +0100

    fixed prediction error

--- src/inference.py
+++ src/inference.py
@@ -30,20 +30,17 @@ def pipeline(text):
 sentence = "A former Deputy Governor 's Central Bank said million Nigerians"
 sentence1 = 'URGENT! We are trying to contact U. Todays draw shows that you have won a å£2000 prize GUARANTEED. Call 09066358361 from land line. Claim Y87. Valid 12hrs only,'
 
-sentence = pipeline(sentence)
-sentence1 = pipeline(sentence)
-
-print(sentence1)
-print(sentence)
+sentence_transform = pipeline(sentence)
+sentence1_transform = pipeline(sentence1)
 
 model = pickle.load(open('models/model_pipeline.pkl', 'rb'))
 
-prediction = model.predict([sentence1])
-prediction_proba = model.predict_proba([sentence1])
+prediction = model.predict([sentence1_transform])
+prediction_proba = model.predict_proba([sentence1_transform])
 if prediction[0] == 0: 
-    prediction = 'not spam'
+    prediction = 'Non Spam Tweet'
 else: 
-    prediction = 'spam'
+    prediction = 'Spam Tweet'
 
-print(prediction)
-print(prediction_proba)
+print(f"{sentence1} is a {prediction}")
+print(f"Model is {round(prediction_proba.max(), 2)}% Acuurate")
